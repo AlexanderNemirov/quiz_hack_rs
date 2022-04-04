545ff8bb1395f62ab52f0d8192f984d6bde47cf1
AlexanderNemirov <alexandernemirov98@gmail.com>
2022-04-04 13:23:21 +0200

    add tests to main

--- src/main.rs
+++ src/main.rs
@@ -27,3 +27,31 @@ fn main() {
         println!("number of solutions: {}", solution.len());
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_words1000() {
+        let path = String::from("data/word_rus1000.txt");
+
+        let letters_str_1 = String::from("тетачасчастьчетачечет");
+        let letters_hash_1 = str_2_hashmap(&letters_str_1);
+        let (vecs_1, _) = read_words(&path, &letters_hash_1);
+        assert_eq!(6, vecs_1.len());
+        assert_eq!(0, gen_main(&vecs_1, &letters_hash_1).len());
+
+        let letters_str_2 = String::from("экономикатеатрновоегорломинистр");
+        let letters_hash_2 = str_2_hashmap(&letters_str_2);
+        let (vecs_2, _) = read_words(&path, &letters_hash_2);
+        assert_eq!(132, vecs_2.len());
+        assert_eq!(4, gen_main(&vecs_2, &letters_hash_2).len());
+
+        let letters_str_3 = String::from("данныелесгодцифраозеро");
+        let letters_hash_3 = str_2_hashmap(&letters_str_3);
+        let (vecs_3, _) = read_words(&path, &letters_hash_3);
+        assert_eq!(66, vecs_3.len());
+        assert_eq!(3, gen_main(&vecs_3, &letters_hash_3).len());
+    }
+}
